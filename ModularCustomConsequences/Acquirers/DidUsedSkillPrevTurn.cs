762baef5f10f500dc5c9a1c87e07260eaa490aca
ANSD_7 <ansddeveloper@gmail.com>
2026-01-20 14:03:15 +0700

    added null value checks

--- ModularCustomConsequences/Acquirers/DidUsedSkillPrevTurn.cs
+++ ModularCustomConsequences/Acquirers/DidUsedSkillPrevTurn.cs
@@ -7,6 +7,7 @@ public class AcquirerDidUsedSkillPrevTurn : IModularAcquirer
     public int ExecuteAcquirer(ModularSA modular, string section, string circledSection, string[] circles)
     {
         BattleUnitModel target = modular.GetTargetModel(circles[0]);
+        if (target == null) return -1;
         int skillId = modular.GetNumFromParamString(circles[1]);
         return target.DidActionPrevTurn(skillId) ? 1 : 0;
     }
--- ModularCustomConsequences/Acquirers/IsActionable.cs
+++ ModularCustomConsequences/Acquirers/IsActionable.cs
@@ -7,6 +7,7 @@ public class AcquirerIsActionable : IModularAcquirer
     public int ExecuteAcquirer(ModularSA modular, string section, string circledSection, string[] circles)
     {
         BattleUnitModel target = modular.GetTargetModel(circles[0]);
+        if (target == null) return -1;
         return target.IsActionable() ? 1 : 0;
     }
 }
